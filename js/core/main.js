ce625471618e90f9979d9a26a5241ef18a701518
xudq789 <616737126@qq.com>
2026-09-07 12:51:26 +0800

    Feat: three birth input modes (solar/lunar/bazi reverse)

--- js/core/main.js
+++ js/core/main.js
@@ -465,7 +465,7 @@ import {
     unlockDownloadButton, resetUnlockInterface, animateButtonStretch,
     showLoadingModal, hideLoadingModal, showAnalysisResult,
     hideAnalysisResult, validateForm, collectUserData
-} from './ui.js?v=21';
+} from './ui.js?v=22';
 
 var _pollState = {
     active: false,

e25617294d412623ccf1615c71b8dad25c001c37
xudq789 <616737126@qq.com>
2026-09-23 11:52:05 +0800

    Default city Beijing + year 1990, SEO robots/sitemap/meta/static content, admin noindex

--- js/core/ui.js
+++ js/core/ui.js
@@ -1,6 +1,6 @@
 // UI控制模块
 import { DOM, formatDate, hideElement, showElement } from './utils.js?v=20';
-import { SERVICES, STATE, API_CONFIG } from './config.js?v=20';
+import { SERVICES, STATE, API_CONFIG } from './config.js?v=21';
 import { reverseBaziFromPillars } from './api.js?v=22';
 
 // UI元素集合
